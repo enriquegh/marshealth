88b695e49a75b53eaabe7cf0464c4f144bf7098b
Bryan Relampagos <barelampagos@gmail.com>
2016-05-06 20:36:58 -0700

    Fixed 12 hour -> 24 hour time conversion

--- web/js/format.js
+++ web/js/format.js
@@ -1,33 +1,67 @@
-function addFilter() {
+// Filter for Grabbing Appointments
+function getApptFilter() {
     var scheduleStartDate = new Date(document.getElementById("startDate").value);
-    scheduleStartDate = scheduleStartDate.getFullYear() + '-' +
-    ('00' + (scheduleStartDate.getMonth()+1)).slice(-2) + '-' +
-    ('00' + scheduleStartDate.getDate()).slice(-2);
-
+    scheduleStartDate = convertDate(scheduleStartDate);
     scheduleStartDate = "date,ge," + scheduleStartDate;
-    console.log(scheduleStartDate);
-    document.getElementById("startDate").value = scheduleStartDate;
 
     var scheduleEndDate = new Date(document.getElementById("endDate").value);
-    scheduleEndDate = scheduleEndDate.getFullYear() + '-' +
-    ('00' + (scheduleEndDate.getMonth()+1)).slice(-2) + '-' +
-    ('00' + scheduleEndDate.getDate()).slice(-2);
-
+    scheduleEndDate = convertDate(scheduleEndDate);
     scheduleEndDate = "date,le," + scheduleEndDate;
-    document.getElementById("endDate").value = scheduleEndDate;
 
-    return false;
+    document.getElementById("startDate").value = scheduleStartDate;
+    document.getElementById("endDate").value = scheduleEndDate;
 }
 
-function dateFormat() {
-  var date = new Date(document.getElementById("scheduleID").value);
+// Filter for setting appointments
+function setApptFilter() {
+  var date = new Date(document.getElementById("apptDate").value);
+  date = convertDate(date);
 
+  var startTime = document.getElementById("startTime").value;
+  var endTime = document.getElementById("endTime").value;
+  var start = convertTime(startTime);
+  var end = convertTime(endTime);
+
+  document.getElementById("apptDate").value = date;
+  document.getElementById("startTime").value = start;
+  document.getElementById("endTime").value = end;
+}
+
+function setSchedFilter() {
+  var date = new Date(document.getElementById("scheduleID").value);
+  date = convertDate(date);
+
+  var openTime = document.getElementById("openTime").value;
+  var closeTime = document.getElementById("closeTime").value;
+  var open = convertTime(openTime);
+  var close = convertTime(closeTime);
+
+  document.getElementById("scheduleID").value = date;
+  document.getElementById("openTime").value = open;
+  document.getElementById("closeTime").value = close;
+}
+
+function convertTime(time) {
+    var timeSplit = time.split(" ");
+
+    if (timeSplit[1] == "PM") {
+      timeSplit = timeSplit[0].split(":");
+      console.log(timeSplit);
+      var fixedTime = parseInt(timeSplit[0]) + 12;
+      fixedTime = fixedTime.toString() + ":" + timeSplit[1];
+      console.log(fixedTime);
+      return fixedTime;
+    } else {
+      return time;
+    }
+}
+
+function convertDate(date) {
   date = date.getFullYear() + '-' +
   ('00' + (date.getMonth()+1)).slice(-2) + '-' +
   ('00' + date.getDate()).slice(-2);
 
-  console.log(date);
-  document.getElementById("scheduleID").value = date;
+  return date;
 }
 
 $('.datepicker').pickadate({
@@ -36,6 +70,7 @@ $('.datepicker').pickadate({
 });
 
 $('.timepicker').pickatime();
+
 $(".button-collapse").sideNav();
 $(document).ready(function(){
     $('.parallax').parallax();
