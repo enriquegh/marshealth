3fa609ae9a46fd1290cc803ffbab333dbb417852
Enrique Gonzalez <enrique@saucelabs.com>
2022-04-14 17:34:02 -0700

    remove unused imports

--- android/app/src/main/java/com/enriquegh/marshealth/LoginActivity.java
+++ android/app/src/main/java/com/enriquegh/marshealth/LoginActivity.java
@@ -1,40 +1,24 @@
 package com.enriquegh.marshealth;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.enriquegh.marshealth.network.LoginErrorResponseListener;
 import com.enriquegh.marshealth.network.LoginResponseListener;
 import com.enriquegh.marshealth.util.BaseURLUtility;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 
 public class LoginActivity extends AppCompatActivity {
